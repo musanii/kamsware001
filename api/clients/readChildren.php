11e7144697926b60d9cc5975510070d562396050
Kevin Musanii <kevin.kibet@stl-horizon.com>
2021-08-12 06:55:29 +0300

    added read clients with children functionality

---
+++ api/clients/readChildren.php
@@ -0,0 +1,57 @@
+<?php
+
+//neccessary headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+//db  and object files
+include_once '../config/database.php';
+include_once '../objects/client.php';
+
+//instantiate necessary  objects
+$database = new Database();
+$db = $database->getConnection();
+
+$client = new Client($db);
+
+//read clients 
+$stmt = $client->readChildren();
+$num = $stmt->rowCount();
+
+//check if more than 0 record found
+
+if($num>0){
+
+    //clients array
+    $clients_arr = array();
+    $clients_arr["records"] = array();
+
+    while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        extract($row);
+
+        $client_item = array(
+            "id" => $id,
+            "firstName"=>$firstName,
+            "lastName"=> $lastName,
+            "dateOfBirth"=> $dateOfBirth,
+            "idNumber"=> $idNumber,
+            "phoneNumber"=> $phoneNumber,
+            "centerName" => $centerName,
+            "locationName"=>$locationName,
+            "productName"=>$name,
+            "description"=>$description,
+            "quantity"=>$quantity
+
+        );
+        array_push($clients_arr["records"], $client_item);
+    }
+    //set response codes here
+    http_response_code(200);
+    echo json_encode($clients_arr);
+
+}else{
+    //not found
+    http_response_code(404);
+
+    echo json_encode(["message"=>"No clients and associated records found."]);
+}
--- api/objects/client.php
+++ api/objects/client.php
@@ -14,6 +14,11 @@ class Client {
     public $dateOfBirth;
     public $idNumber;
     public $phoneNumber;
+    public $centerName;
+    public $locationName;
+    public $name;
+    public $description;
+    public $quantity;
 
     //constructor with $db as database connection
 public function __construct($db)
@@ -24,7 +29,8 @@ public function __construct($db)
 
 //read  clients 
 
-function read(){
+function read()
+{
 
     $query = "SELECT *  FROM " . $this->table_name . " ORDER BY createdAt DESC";
 
@@ -34,6 +40,32 @@ function read(){
     //execute
     $stmt->execute();
     return $stmt;
+}
+
+//read cients with children
+function readChildren()
+{
+    $query = "SELECT
+     cl.id, cl.firstName,cl.lastName,cl.dateOfBirth,cl.idNumber,cl.phoneNumber, ce.centerName as centerName,ce.locationName as locationName, p.name as name, p.description as description, p.quantity as quantity
+FROM
+    " . $this->table_name . " cl
+    LEFT JOIN
+        centers ce
+            ON cl.id = ce.client_id
+    LEFT JOIN
+    products p
+    ON cl.id = p.client_id
+GROUP BY
+    cl.id ";
+
+    // prepare query statement
+    $stmt = $this->conn->prepare($query);
+  
+    // execute query
+    $stmt->execute();
+  
+    return $stmt;
+
 }
  
 }
